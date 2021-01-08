2c839acc062a406da68958c3d830e128df3536a9
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-01-08 23:40:00 +1100

    Add files via upload

--- ttsbot.js
+++ ttsbot.js
@@ -30,61 +30,60 @@ const vocodesVoices = {
 	arnold: "arnold-schwarzenegger",
 	attenborough: "david-attenborough",
 	ayoade: "richard-ayoade",
+	barker: "bob-barker",
 	bart: "bart-simpson",
-	ben_stein: "ben-stein",
-	betty_white: "betty-white",
-	bill_clinton: "bill-clinton",
-	bill_gates: "bill-gates",
-	bill_nye: "bill-nye",
-	bob_barker: "bob-barker",
+	bill: "bill-clinton",
 	boss: "the-boss",
 	brimley: "wilford-brimley",
 	broomstick: "boomstick",
 	bush: "george-w-bush",
 	carter: "jimmy-carter",
-	christopher_lee: "christopher-lee",
 	cooper: "anderson-cooper",
-	craig_ferguson: "craig-ferguson",
 	cramer: "jim-cramer",
 	cranston: "bryan-cranston",
-	crypt_keeper: "crypt-keeper",
+	cross: "david-cross",
 	darth: "darth-vader",
-	david_cross: "david-cross",
+	deen: "paula-deen",
 	degrasse: "neil-degrasse-tyson",
 	dench: "judi-dench",
 	devito: "danny-devito",
-	dr_phil: "dr-phil-mcgraw",
-	earl_jones: "james-earl-jones",
-	fred_rogers: "fred-rogers",
+	ferguson: "craig-ferguson",
+	gates: "bill-gates",
 	gottfried: "gilbert-gottfried",
-	hillary_clinton: "hillary-clinton",
+	graham: "paul-graham",
+	hillary: "hillary-clinton",
 	homer: "homer-simpson",
+	jones: "james-earl-jones",
+	keeper: "crypt-keeper",
+	king: "larry-king",
 	krabs: "mr-krabs",
-	larry_king: "larry-king",
+	lee: "christopher-lee",
 	lisa: "lisa-simpson",
 	luckey: "palmer-luckey",
 	mcconnell: "mitch-mcconnell",
 	nimoy: "leonard-nimoy",
 	nixon: "richard-nixon",
+	nye: "bill-nye",
 	obama: "barack-obama",
 	oliver: "john-oliver",
 	palin: "sarah-palin",
-	paul_graham: "paul-graham",
-	paula_deen: "paula-deen",
 	penguinz0: "moistcr1tikal",
+	phil: "dr-phil-mcgraw",
 	reagan: "ronald-reagan",
 	rickman: "alan-rickman",
+	rogers: "fred-rogers",
 	rosen: "michael-rosen",
 	saruman: "saruman",
-	scout: "scout",
+	scout2: "scout",
 	shapiro: "ben-shapiro",
 	shohreh: "shohreh-aghdashloo",
 	simmons: "j-k-simmons",
 	snake: "solid-snake",
 	snape: "severus-snape",
 	sonic: "sonic",
-	spongebob: "spongebob-squarepants",
+	spongebob2: "spongebob-squarepants",
 	squidward: "squidward",
+	stein: "ben-stein",
 	takei: "george-takei",
 	thiel: "peter-thiel",
 	trevor: "trevor-philips",
@@ -92,28 +91,95 @@ const vocodesVoices = {
 	tucker: "tucker-carlson",
 	tupac: "tupac-shakur",
 	vegeta: "vegeta",
+	white: "betty-white",
 	wiseau: "tommy-wiseau",
 	wizard: "wizard",
 	yugi: "yami-yugi",
 	zuckerberg: "mark-zuckerberg"
 }
 
-function vocodesSpeak(message, utterance, speaker) {
+const fifteenAIVoices = {
+	adagio: "Adagio Dazzle",
+	administrator: "Administrator",
+	applejack: "Applejack",
+	aria: "Aria Blaze",
+	bloom: "Apple Bloom",
+	bon: "Bon Bon",
+	braeburn: "Braeburn",
+	cadance: "Princess Cadance",
+	carl: "Carl Brutananadilewski",
+	celestia: "Princess Celestia",
+	cheerilee: "Cheerilee",
+	chrysalis: "Queen Chrysalis",
+	coco: "Coco Pommel",
+	cozy: "Cozy Glow",
+	dan: "Dan",
+	daria: "Daria Morgendorffer",
+	daring: "Daring Do",
+	dash: "Rainbow Dash",
+	demoman: "Demoman",
+	derpy: "Derpy Hooves",
+	diamond: "Diamond Tiara",
+	discord: "Discord",
+	engineer: "Engineer",
+	fluttershy: "Fluttershy",
+	gabby: "Gabby",
+	gilda: "Gilda",
+	glados: "GLaDOS",
+	hal: "HAL 9000",
+	heavy: "Heavy",
+	jane: "Jane Lane",
+	kyu: "Kyu Sugardust",
+	lightning: "Lightning Dust",
+	limestone: "Limestone Pie",
+	luna: "Princess Luna",
+	lyra: "Lyra",
+	mac: "Big Mac",
+	maud: "Maud Pie",
+	medic: "Medic",
+	minuette: "Minuette",
+	moondancer: "Moondancer",
+	narrator: "The Narrator",
+	octavia: "Octavia",
+	pauling: "Miss Pauling",
+	pinkie: "Pinkie Pie",
+	prince: "Shining Armor",
+	rarity: "Rarity",
+	rise: "Rise Kujikawa",
+	scootaloo: "Scootaloo",
+	scout: "Scout",
+	silver: "Silver Spoon",
+	snails: "Snails",
+	sniper: "Sniper",
+	snips: "Snips",
+	soarin: "Soarin'",
+	soldier: "Soldier",
+	sonata: "Sonata Dusk",
+	spike: "Spike",
+	spitfire: "Spitfire",
+	spongebob: "SpongeBob SquarePants",
+	spy: "Spy",
+	starlight: "Starlight Glimmer",
+	steven: "Steven Universe",
+	sugar: "Sugar Belle",
+	sunburst: "Sunburst",
+	sunset: "Sunset Shimmer",
+	sweetie: "Sweetie Belle",
+	tennant: "Tenth Doctor",
+	trixie: "Trixie",
+	turret: "Sentry Turret",
+	twilight: "Twilight Sparkle",
+	vapor: "Vapor Trail",
+	wheatley: "Wheatley",
+	zecora: "Zecora"
+}
+
+function speak(message, utterance, params) {
 	if (utterance) {
 		console.log("Playing " + utterance + "!");
 		// Generate random temporary filename to avoid overwriting other speech recordings
 		const fileName = crypto.randomBytes(48).toString("hex") + ".wav";
-		request.post({
-			url: "https://mumble.stream/speak",
-			headers: {
-				"Accept": "application/json",
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				speaker: speaker,
-				text: utterance
-			}),
-		}, function(error, response, body) {
+		request.post(params, function(error, response, body) {
 			if (error) {
 				console.error(error);
 				// Delete temporary file upon error
@@ -176,9 +242,34 @@ client.on("message", function(message) {
 			break;
 		default:
 			const command = trimmedContent.split(" ")[0];
+			const utterance = message.content.slice(4 + command.length).trim();
+			const fifteenAIVoice = fifteenAIVoices[command];
 			const vocodeVoice = vocodesVoices[command];
-			if (vocodeVoice) {
-				vocodesSpeak(message, message.content.slice(4 + command.length).trim(), vocodeVoice);
+			if (fifteenAIVoice) {
+				speak(message, utterance, {
+					url: "https://api.15.ai/app/getAudioFile",
+					headers: {
+						"Accept": "application/json",
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						character: fifteenAIVoice,
+						text: utterance,
+						emotion: "Contextual"
+					}),
+				});
+			} else if (vocodeVoice) {
+				speak(message, utterance, {
+					url: "https://mumble.stream/speak",
+					headers: {
+						"Accept": "application/json",
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						speaker: vocodeVoice,
+						text: utterance
+					}),
+				});
 			} else {
 				message.channel.send("No voice named " + command + "!").catch(console.error);
 			}
